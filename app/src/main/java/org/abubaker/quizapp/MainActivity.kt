c14bdfb3c0a15236a70ff69436479d9ecc941265
abubaker <mohammad_abubaker@yahoo.com>
2021-04-09 20:34:05 +0500

    Implemented DataBinding

--- app/src/main/java/org/abubaker/quizapp/MainActivity.kt
+++ app/src/main/java/org/abubaker/quizapp/MainActivity.kt
@@ -2,10 +2,16 @@ package org.abubaker.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.abubaker.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
