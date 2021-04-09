b0e57b94ca945ee0ac77f4850318b82279d0d6fd
abubaker <mohammad_abubaker@yahoo.com>
2021-04-10 02:01:17 +0500

    Added flags (images) in the drawable

---
+++ app/src/main/java/org/abubaker/quizapp/Constants.kt
@@ -0,0 +1,12 @@
+package org.abubaker.quizapp
+
+object Constants {
+
+    fun getQuestions(): ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+
+        // Data Set
+        // val que1 = Question(1, "What country does this flag belongs to?")
+    }
+
+}
---
+++ app/src/main/java/org/abubaker/quizapp/Question.kt
@@ -0,0 +1,13 @@
+package org.abubaker.quizapp
+
+// data class = it will nave not functions
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: String
+)
