b657ef81e1df09f2980f0fc2956b49ab11193896
abubaker <mohammad_abubaker@yahoo.com>
2021-04-11 00:33:25 +0500

    Optimizing code...

--- app/src/main/java/org/abubaker/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/org/abubaker/quizapp/QuizQuestionsActivity.kt
@@ -1,8 +1,12 @@
 package org.abubaker.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import org.abubaker.quizapp.data.Constants
 import org.abubaker.quizapp.data.Question
@@ -14,6 +18,16 @@ class QuizQuestionsActivity : AppCompatActivity() {
     // Binding Object
     private lateinit var binding: ActivityQuizQuestionsBinding
 
+    // Current Question
+    private var mCurrentPosition: Int = 1
+
+    // It will make questions list global
+    private var mQuestionsList: ArrayList<Question>? = null
+
+    // Selected answer from the 4 options.
+    private var mSelectedOptionPosition: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,12 +38,18 @@ class QuizQuestionsActivity : AppCompatActivity() {
         val questionsList = Constants.getQuestions()
         Log.i("Questions Size: ", "${questionsList.size}")
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
+        setQuestion()
 
-        binding.progressBar.progress = currentPosition
+    }
 
-        binding.tvProgress.text = "$currentPosition / ${binding.progressBar.max}"
+    private fun setQuestion() {
+
+        mCurrentPosition = 1
+        val question = mQuestionsList!!.get(mCurrentPosition - 1)
+
+        binding.progressBar.progress = mCurrentPosition
+
+        binding.tvProgress.text = "$mCurrentPosition / ${binding.progressBar.max}"
         binding.tvQuestion.text = question!!.question
 
         binding.ivImage.setImageResource(question.image)
@@ -38,6 +58,32 @@ class QuizQuestionsActivity : AppCompatActivity() {
         binding.tvOptionTwo.text = question.optionTwo
         binding.tvOptionThree.text = question.optionThree
         binding.tvOptionFour.text = question.optionFour
+
+    }
+
+    //
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        // For loop will update UI based on updated options
+        for (option in options) {
+
+            // Text Color
+            option.setTextColor(Color.parseColor("7A8089"))
+
+            // Typeface
+            option.typeface = Typeface.DEFAULT
+
+            // Border Color
+            option.background =
+                ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+
+
+        }
 
 
     }
